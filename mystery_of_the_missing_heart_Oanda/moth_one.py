74982d21d28dde4058e20631b7b5bc1ec62b73e9
quantatc <anstochibamy@gmail.com>
2023-06-26 08:40:40 +0400

    oanda lotsize

--- mystery_of_the_missing_heart_Oanda/moth_one.py
+++ mystery_of_the_missing_heart_Oanda/moth_one.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     last_action_timestamp = 0
     last_display_timestamp = 0
 
-    trader = MysteryOfTheMissingHeart(symbols, lot_size=0.05)
+    trader = MysteryOfTheMissingHeart(symbols, lot_size=0.03)
 
     while True:
         # Launch the algorithm
