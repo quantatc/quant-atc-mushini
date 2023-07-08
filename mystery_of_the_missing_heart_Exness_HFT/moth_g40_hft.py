605659843cb8f7f47033339d376a8c88b3f44d83
quantatc <anstochibamy@gmail.com>
2023-07-08 17:22:50 +0200

    changed the while loop for all

--- mystery_of_the_missing_heart_Exness_HFT/moth_g40_hft.py
+++ mystery_of_the_missing_heart_Exness_HFT/moth_g40_hft.py
@@ -190,30 +190,31 @@ if __name__ == "__main__":
     while True:
         # Launch the algorithm
         current_timestamp = int(time.time())
-        if (current_timestamp - last_action_timestamp) > 300: #changed to 300 from 3600
-            if datetime.now().weekday() not in (5,6):
-                if not 20 <= datetime.now().hour < 1:
-                    # Account Info
-                    if mt5.initialize(login=mt_login_id, server=mt_server_name, password=mt_password):
-                        current_account_info = mt5.account_info()
-                        print("------------------------------------------------------------------------------------------")
-                        print("EXNESS: GER40 HFT-LIKE STRATEGY")
-                        print("------------------------------------------------------------------------------------------")
-                        print(f"Date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-                        if current_account_info is not None:
-                            print(f"Balance: {current_account_info.balance} USD,\t"
-                                  f"Equity: {current_account_info.equity} USD, \t"
-                                  f"Profit: {current_account_info.profit} USD")
-                        else:
-                            print("Failed to retrieve account information.")
-                        print("-------------------------------------------------------------------------------------------")
-                    # Look for trades
-                    trader.execute_trades()
-            last_action_timestamp = int(time.time())
-        
-        if (current_timestamp - last_display_timestamp) > 300:
+        current_datetime = datetime.now()
+
+        if (current_timestamp - last_action_timestamp) > 3600:  # changed to 60 from 3600
+            if current_datetime.weekday() < 5:  # Monday to Friday
+                # Account Info
+                if mt5.initialize(login=mt_login_id, server=mt_server_name, password=mt_password):
+                    current_account_info = mt5.account_info()
+                    print("------------------------------------------------------------------------------------------")
+                    print("MOTH MOMENTUM STRATEGY: EXNESS HFT ACCOUNT")
+                    print("------------------------------------------------------------------------------------------")
+                    print(f"Date: {current_datetime.strftime('%Y-%m-%d %H:%M:%S')}")
+                    if current_account_info is not None:
+                        print(f"Balance: {current_account_info.balance} USD,\t"
+                                f"Equity: {current_account_info.equity} USD, \t"
+                                f"Profit: {current_account_info.profit} USD")
+                    else:
+                        print("Failed to retrieve account information.")
+                    print("-------------------------------------------------------------------------------------------")
+                # Look for trades
+                trader.execute_trades()
+                last_action_timestamp = int(time.time())
+
+        if (current_timestamp - last_display_timestamp) > 3600:
             trader.check_position()
             last_display_timestamp = int(time.time())
-           
-        # to avoid excessive cpu usage because loop running lightning fast
-        sleep(5)
+
+        # to avoid excessive CPU usage because the loop is running too fast
+        time.sleep(5)
--- mystery_of_the_missing_heart_Oanda/moth_jpy.py
+++ mystery_of_the_missing_heart_Oanda/moth_jpy.py
@@ -200,14 +200,18 @@ if __name__ == "__main__":
     while True:
         # Launch the algorithm
         current_timestamp = int(time.time())
-        if (current_timestamp - last_action_timestamp) > 3600: #changed to 60 from 3600
-            if datetime.now().weekday() not in (5,6):
-                if not 23 <= datetime.now().hour <= 3:
+        current_datetime = datetime.now()
+
+        if (current_timestamp - last_action_timestamp) > 3600:  # changed to 60 from 3600
+            if current_datetime.weekday() < 5:  # Monday to Friday
+                if not (23 <= current_datetime.hour <= 3):  # Not between 11 PM and 3 AM
                     # Account Info
                     if mt5.initialize(login=mt_login_id, server=mt_server_name, password=mt_password):
                         current_account_info = mt5.account_info()
                         print("------------------------------------------------------------------------------------------")
-                        print(f"Date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+                        print("MOTH CORR JPY: OANDA LIVE ACCOUNT")
+                        print("------------------------------------------------------------------------------------------")
+                        print(f"Date: {current_datetime.strftime('%Y-%m-%d %H:%M:%S')}")
                         if current_account_info is not None:
                             print(f"Balance: {current_account_info.balance} USD,\t"
                                   f"Equity: {current_account_info.equity} USD, \t"
@@ -217,11 +221,11 @@ if __name__ == "__main__":
                         print("-------------------------------------------------------------------------------------------")
                     # Look for trades
                     trader.execute_trades()
-            last_action_timestamp = int(time.time())
-        
+                last_action_timestamp = int(time.time())
+
         if (current_timestamp - last_display_timestamp) > 3600:
             trader.check_position()
             last_display_timestamp = int(time.time())
-           
-        # to avoid excessive cpu usage because loop running lightning fast
-        sleep(30)
+
+        # to avoid excessive CPU usage because the loop is running too fast
+        time.sleep(5)
