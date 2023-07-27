a103ea5f662a7396c93f3f3c5bcd7f215cffed71
quantatc <anstochibamy@gmail.com>
2023-07-27 22:42:37 +0200

    tick is none error handling

--- mystery_of_the_missing_heart_MFF/moth_forex.py
+++ mystery_of_the_missing_heart_MFF/moth_forex.py
@@ -86,6 +86,9 @@ class MysteryOfTheMissingHeart:
         deviation = 20
         tick = mt5.symbol_info_tick(symbol)
         symbol_info = mt5.symbol_info(symbol)
+        if tick == None:
+            logging.error(f'order_send failed, error code={mt5.last_error()}')
+            return False
         
         if order_type == mt5.ORDER_TYPE_BUY:
             price = tick.ask
