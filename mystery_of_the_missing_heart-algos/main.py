6021259f3b8090f91801ee95c7e20efde4014abe
quantatc <anstochibamy@gmail.com>
2023-11-23 08:18:10 +0000

    important

--- mystery_of_the_missing_heart-algos/main.py
+++ mystery_of_the_missing_heart-algos/main.py
@@ -11,10 +11,10 @@ env_path = r"C:\Users\anstochibamu\Documents\GitHub\quant-atc-mushini\mystery_of
 directory = r"C:\Users\anstochibamu\Documents\GitHub\quant-atc-mushini\mystery_of_the_missing_heart-algos"
 
 # Create threads for each script
-thread1 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_xm.py", env_path))
-thread2 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_oanda.py", env_path))
-thread3 = threading.Thread(target=run_script, args=(directory + r"\moth_volatility_deriv.py", env_path))
-thread4 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_binance.py", env_path))
+# thread1 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_xm.py", env_path))
+thread1 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_oanda.py", env_path))
+thread2 = threading.Thread(target=run_script, args=(directory + r"\moth_volatility_deriv.py", env_path))
+thread3 = threading.Thread(target=run_script, args=(directory + r"\moth_scalping101_binance.py", env_path))
 
 # Start threads
 thread1.start()
@@ -24,11 +24,11 @@ thread2.start()
 time.sleep(3)
 thread3.start()
 
-time.sleep(3)  # Delay to prevent clashes
-thread4.start()
+# time.sleep(3)  # Delay to prevent clashes
+# thread4.start()
 
 # Join threads
 thread1.join()
 thread2.join()
 thread3.join()
-thread4.join()
+# thread4.join()
